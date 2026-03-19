039481299b7a54e0e553fc26242ace2e0c968d02
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-03-19 17:03:35 -0300

    operadores relacionais

---
+++ aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo restrito
+
+let nome="Bruno"
+console.log(nome)
+console.log("CBF Cursos")
+console.log("Nome: " + nome)
---
+++ aula03.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let nome="Bruno"
+nome="CBF Cursos"
+nome=10
+
+const curso="JavaScript"
+
+curso = "HTML"
+
+console.log(curso)
+
