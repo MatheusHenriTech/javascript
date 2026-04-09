6d7d580b6686a242fda3b0ea05c8d05f574df600
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-04-09 17:30:45 -0300

    estudos

---
+++ aula61.js
@@ -0,0 +1,26 @@
+const Pessoa={
+    nome,
+    idade,
+    getNome:function(){
+        return this.nome
+    },
+    getIdade:function(){
+        return this.idade
+    },
+    setNome:function(nome){
+        this.nome=nome
+    },
+    setIdade:function(idade){
+        this.idade=idade
+    },
+}
+
+const p2=Pessoa
+const p3=Pessoa
+
+p3.nome="Cladisvardson"
+p2["nome"]="Bridgertrudismelda"
+Pessoa.setNome("Patricscrerdison")
+
+console.log(Pessoa.nome)
+console.log(p2.getNome())
