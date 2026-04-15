f738effe0435431bb6821d9dc377470570d517f7
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-04-15 17:21:11 -0300

    exercício

--- aula77.js
+++ aula77.js
@@ -1,13 +1,32 @@
 const div_data=document.getElementById("div_data")
+const div_relogio=document.getElementById("div_relogio")
+const som_alarme=document.getElementById("som_alarme")
+const btn_ativar=document.getElementById("btn_ativar")
+const btn_parar=document.getElementById("btn_parar")
+const tmp_alarme=document.getElementById("tmp_alarme")
+const hora_alarme=document.getElementById("hora_alarme")
+const timer=document.getElementById("timer")
+
 const data=new Date()
 
 let dia=data.getDate()
-dia=dia<10?"0"+dia_m:dia_m
-
+dia=dia<10?"0"+dia:dia
 let mes=data.getMonth()
 mes=mes<10?"0"+mes:mes
-
-const data_r=dia_m+"/"+mes+"/"+data.getFullYear()
-
+const data_r=dia+"/"+mes+"/"+data.getFullYear()
 div_data.innerHTML=data_r
+
+const relogio=()=>{
+    const data=new Date()
+    let hora=data.getHours()
+    hora=hora<10?"0"+hora:hora
+    let minuto=data.getMinutes()
+    minuto=minuto<10?"0"+minuto:minuto
+    let segundo=data.getSeconds()
+    segundo=segundo<10?"0"+segundo:segundo
+    const hora_completa=hora+":"+minuto+":"+segundo
+    div_relogio.innerHTML=hora_completa
+}
+
+const intervalo=setInterval(relogio,1000)
 
---
+++ aula80.js
@@ -0,0 +1,4 @@
+const mat=document.getElementById("mat")
+
+
+mat.innerHTML=Math.pow(10, 3)
