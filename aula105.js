36055fe2d9bae5dfca479e1512de5c40e0ba0306
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-04-23 17:14:12 -0300

    js

--- aula105.js
+++ aula105.js
@@ -1,4 +1,6 @@
-let nome=new String("Bruno Pinho Campos")
+let nome=new String("Bruno Pinho Campos 1978")
+let email="bruno@bruno.com.br"
+
 console.log(nome)
 
 console.log(nome.search(/pinho/i))
@@ -6,3 +8,10 @@ console.log(nome.search(/pinho/i))
 console.log(nome.match(/O/ig))
 
 console.log(nome.replace(/o/ig, "Teste"))
+
+console.log(nome.match(/\d/ig))
+console.log(nome.match(/\s/ig))
+console.log(nome.match(/\bP/ig))
+
+
+console.log(nome.match(/no*/ig))
