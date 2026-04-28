bbec02adaa3e0232fa377db18ca71491427cc04b
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-04-28 18:17:02 -0300

    .

--- aula129.js
+++ aula129.js
@@ -2,6 +2,8 @@ const timer=document.querySelector("#timer")
 const btn_iniciar=document.querySelector("#btn_iniciar")
 const btn_parar=document.querySelector("#btn_parar")
 const btn_zerar=document.querySelector("#btn_zerar")
+const btn_parcial=document.querySelector("#btn_parcial")
+const parciaisregistradas=document.querySelector("#parciaisregistradas")
 
 let intervalo=null
 let tmpini=null
@@ -29,6 +31,11 @@ btn_iniciar.addEventListener("click",(evt)=>{
     intervalo=setInterval(contador, 1000)
 })
 
+btn_parcial.addEventListener("click",(evt)=>{
+    let parcial="<div>"+timer.innerHTML+"</div>"
+    parciaisregistradas.innerHTML+=parcial
+})
+
 btn_parar.addEventListener("click",(evt)=>{
     clearInterval(intervalo)
 })
@@ -37,4 +44,5 @@ btn_zerar.addEventListener("click",(evt)=>{
     tmpini=Date.now()
     timer.innerHTML="00:00:00"
     clearInterval(intervalo)
+    parciaisregistradas.innerHTML=""
 })
