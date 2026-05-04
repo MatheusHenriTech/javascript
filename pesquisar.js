66a51bbdd7bc4497a6248f9c482d870778b942fb
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-05-04 17:00:05 -0300

    projeto agenda

---
+++ pesquisar.js
@@ -0,0 +1,12 @@
+const btn_pesq=document.querySelector("#btn_pesq");
+const f_txtpesq=document.querySelector("#f_txtpesq");
+
+btn_pesq.addEventListener("click", (evt)=>{
+    if(f_txtpesq.value==""){
+        alert("Digite a pesquisa");
+        f_txtpesq.focus();
+        return;
+    }
+    console.log(document.getElementsByName("f_pesq").value);
+})
+
